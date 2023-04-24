5f25ca76ae98181692a603c51c56e12e3e7536b2
EgorKotl <131181604+EgorKotl@users.noreply.github.com>
2023-04-24 22:18:01 +0500

    Update terrar.py

--- terrar.py
+++ terrar.py
@@ -3,9 +3,9 @@ import random
 from colors import *
 from terrar_mobs import *
 
-
-
-
+all_sprites = pygame.sprite.Group()
+player = Player(WIDTH // 2 // TILE * TILE, HEIGHT // 2 // TILE * TILE)
+all_sprites.add(player)
 running = True
 while running:
     clock.tick(FPS)
@@ -15,7 +15,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         # if event.type == pygame.MOUSEMOTION:
-        #     print(pygame.Vector2(event.pos), player.pos)
+        #     print(pygame.Vector2(event.pos), player.pos,'aa')
     all_sprites.draw(screen)
     all_sprites.update()
     mapping()
