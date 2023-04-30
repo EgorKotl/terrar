dcaef929cee691d99086be08a53283869d3d33a6
EgorKotl <131181604+EgorKotl@users.noreply.github.com>
2023-04-30 12:43:55 +0500

    Update terrar.py

--- terrar.py
+++ terrar.py
@@ -2,6 +2,7 @@ import pygame as pg
 import random
 from colors import *
 from terrar_mobs import *
+from terrar_inventory import *
 
 all_sprites = pg.sprite.Group()
 spawn = [SC_WIDTH / 2, SC_HEIGHT / 2]
@@ -9,11 +10,19 @@ player = Player(spawn[0], spawn[1])
 all_sprites.add(player)
 running = True
 camera = pg.Vector2(0, 0)
-
+mode = 0
+kdinv = 10
+invent = inventory()
 while running:
     clock.tick(FPS)
     screen.fill(LIGHT_BLUE)
-
+    pressed = pg.key.get_pressed()
+    kdinv += 1
+    if kdinv >= 10 and pressed[pg.K_ESCAPE]:
+        mode += 1
+        mode %= 2
+        kdinv = 0
+        invent.change_mode(mode)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
@@ -24,8 +33,9 @@ while running:
     for s in all_sprites:
         screen.blit(s.image, s.rect.move(camera))
     # all_sprites.draw(screen)
-    contur(f"Здоровье: {player.hp}/{player.maxhp}", WHITE, BLACK, SC_WIDTH - 200, 1, 1.5)
     mapping(camera, player)
+    invent.draw(screen)
+    contur(f"Здоровье: {player.hp}/{player.maxhp}", WHITE, BLACK, SC_WIDTH - 200, 1, 1.5)
     pg.display.flip()
 
 pg.quit()
