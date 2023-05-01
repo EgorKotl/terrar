2ecc395f628be15f0fd683fa768511582578b7cf
EgorKotl <131181604+EgorKotl@users.noreply.github.com>
2023-05-01 19:13:33 +0500

    Update terrar.py

--- terrar.py
+++ terrar.py
@@ -4,6 +4,16 @@ from colors import *
 from terrar_mobs import *
 from terrar_inventory import *
 
+
+def place_block(type):
+    x, y = player.pos + mouse_pos - (SC_WIDTH / 2, SC_HEIGHT / 2)
+    global map
+    # print(x, y, player.pos)
+    if (player.pos[0] - x) ** 2 + (player.pos[1] - y) ** 2 <= 10000:
+        if not map[int(x) // TILE][int(y) // TILE]:
+            map[int(x) // TILE][int(y) // TILE] = type
+
+
 all_sprites = pg.sprite.Group()
 spawn = [SC_WIDTH / 2, SC_HEIGHT / 2]
 player = Player(spawn[0], spawn[1])
@@ -12,7 +22,10 @@ running = True
 camera = pg.Vector2(0, 0)
 mode = 0
 kdinv = 10
+is_placing = False
 invent = inventory()
+mouse_pos = [0, 0]
+
 while running:
     clock.tick(FPS)
     screen.fill(LIGHT_BLUE)
@@ -26,14 +39,25 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        # if event.type == pygame.MOUSEMOTION:
-        #     print(pygame.Vector2(event.pos), player.pos,'aa')
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                is_placing = True
+        if event.type == pg.MOUSEBUTTONUP:
+            if event.button == 1:
+                is_placing = False
+    if is_placing:
+        place_block(invent.get_element(0, invent.tec)[0])
+    mouse_pos = pg.mouse.get_pos()
     all_sprites.update()
     camera += player.now
     for s in all_sprites:
         screen.blit(s.image, s.rect.move(camera))
     # all_sprites.draw(screen)
     mapping(camera, player)
+    pg.draw.rect(screen, YELLOW,
+                 ((mouse_pos[0] - camera[0] % TILE) // TILE * TILE + camera[0] % TILE,
+                  (mouse_pos[1] - camera[1] % TILE) // TILE * TILE + camera[1] % TILE, TILE,
+                  TILE), 2)
     invent.draw(screen)
     contur(f"Здоровье: {player.hp}/{player.maxhp}", WHITE, BLACK, SC_WIDTH - 200, 1, 1.5)
     pg.display.flip()
